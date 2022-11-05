f41783ff67a930cec231670f98cf91165d250660
hclogic <2822658385@qq.com>
2022-11-05 20:55:48 +0800

    加油

---
+++ test_11_05.cpp
@@ -0,0 +1,107 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf_s("%d%d%d", &a, &b, &c);
+//	if (a < b)
+//	{
+//		int  tmp = a;
+//		a = b;
+//		b = tmp;
+//	}
+//	if (a < c)
+//	{
+//		int tmp = a;
+//		a = c;
+//		c = tmp;
+//
+//	}
+//	if (b < c)
+//	{
+//		int tmp = b;
+//		b = c;
+//		c = tmp;
+//	}
+//
+//	printf("%d %d %d\n", a, b, c);
+//	return 0;
+//
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 3 == 0)
+//		{
+//			printf("%d  ", i);
+//		}
+//	}
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 3; i <= 100; i +=3)
+//	{
+//		printf("%d  ", i);
+//	}
+//	return 0;
+//}
+
+
+//int main()//求两个数最大公约数
+//{
+//	int m = 0;
+//	int n = 0;
+//	scanf_s("%d%d", &m, &n);
+//	int max = 0;
+//	if (m > n)
+//		max = n;
+//	else
+//		max = m;
+//	while (1)
+//	{
+//		if (m % max == 0 && n % max == 0)
+//		{
+//			printf("最大公约数是%d\n", max);
+//			break;
+//		}
+//		max--;
+//	}
+//	
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n = 0;
+//	int m = 0;
+//	scanf_s("%d%d", &m, &n);
+//	int t = 0;
+//	while (t = m % n)
+//	{
+//		m = n;
+//		n = t;
+//	}
+//	printf("%d\n", n);
+//
+//	return 0;
+//}
+
+
+int main()
+{
+
+	return =0
+}
