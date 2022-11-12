3354ee634ce2d1e58afa48795c2cfbeecd42d826
hclogic <2822658385@qq.com>
2022-11-12 23:09:15 +0800

    加油

---
+++ test_11_12.cpp
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <string.h>
+
+//int main()
+//{
+//	char arr1[20] = { 0 };
+//	char arr2[] = "hello bit";
+//	strcpy_s (arr1, arr2);//拷贝字符串
+//	printf("%s", arr1);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = "hello bit";
+//	memset(arr1, 'x', 5);//字符串前5个字符设置成x
+//	printf("%s", arr1);
+//	return 0;
+//}
+
+
+//int max(int x, int y)
+//{
+//	int z = 0;
+//	if (x > y)
+//		z = x;
+//	else
+//		z = y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = max(a, b);
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+
+//void jiaohuan(int x, int y)//void表示这个函数不返回任何值，也不需要返回。
+//{
+//	int z = 0;
+//	z = x;
+//	x = y;
+//	y = z;	
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	jiaohuan(a, b);
+//		printf("%d  %d", a, b);
+//	return 0;
+//}//交换失败
+
+
+//void jiaohuan(int *pa, int *pb)//通过指针地址交换//函数定义//形参
+//{
+//	int z = 0;
+//	z = *pa;
+//	*pa = *pb;
+//	*pb = z;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	printf("%d %d\n", a, b);
+//	jiaohuan (&a, &b);//调用函数//实参
+//	printf("%d %d\n", a, b);
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
